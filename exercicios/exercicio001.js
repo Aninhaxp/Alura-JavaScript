db48fe70d7fdb22bef146e79884bd4a86f640004
Ana Clara Silva Morais <aninhaatrabalho01@gmail.com>
2026-05-18 13:11:35 -0300

    exercicio 002 finalizado em JS

--- exercicios/exercicio001.js
+++ exercicios/exercicio001.js
@@ -1,2 +1,4 @@
+//Crie uma variável chamada nome e atribua seu nome completo. Depois, use console.log() para exibir a mensagem: "Olá, [nome]! Seja bem-vindo(a) ao curso de JavaScript!
+
 let nome = "Ana Clara Silva Morais";
 console.log(`Ola, ${nome}! Seja bem-vindo(a) ao curso de JavaScript`);
---
+++ exercicios/exercicio002.js
@@ -0,0 +1,7 @@
+//Crie duas variáveis: anoAtual e anoNascimento. Calcule a idade da pessoa subtraindo os valores e exiba no console: "Você tem [idade] anos.
+
+let anoAtual = 2026;
+let anoNascimento = 2007;
+ 
+let resultado = anoAtual - anoNascimento;
+console.log(`Você tem ${resultado} anos.`);
