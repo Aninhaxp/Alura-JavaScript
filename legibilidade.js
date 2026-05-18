fc3c35b27d49610c2aa4689d08883f4ffae17296
Ana Clara Silva Morais <aninhaatrabalho01@gmail.com>
2026-05-18 12:48:42 -0300

    entendendo sobre legibilidade

---
+++ legibilidade.js
@@ -0,0 +1,24 @@
+let alturaDaAna = 1.62;
+
+console.log(alturaDaAna);
+
+let altura = 7;
+let largura = 10;
+let area = 70;
+
+//letra maiusculas e minusculas fazem diferença
+
+let preco = 10;
+let Preco = 20;
+
+console.log(preco);
+console.log(Preco);
+//não funciona: console.log("prec0" , prec0)
+
+let idadeUsuario = 20;;
+let nomeCompleto = "Ana Clara Silva Morais";
+
+// Não sabemos do que se trata
+
+let a = 1;
+let b = false;
