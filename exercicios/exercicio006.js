44b13b455a632499e260c50e4187730caaa90f08
Ana Clara Silva Morais <aninhaatrabalho01@gmail.com>
2026-05-20 19:07:13 -0300

    Finalizado exercicio006 em JS

---
+++ exercicios/exercicio006.js
@@ -0,0 +1,16 @@
+//Crie variáveis para três disciplinas: matematica, portugues e ciencias, contendo suas respectivas notas. Calcule e mostre a média final com uma mensagem.
+
+let matematica = 8.5
+let portugues = 7.0
+let ciencias = 9.0
+
+let somaDaMedia = matematica + portugues + ciencias
+let mediaFinal = somaDaMedia / 3
+
+console.log(`
+    Suas notas foram: 
+    Matemática: ${matematica}
+    Português: ${portugues}
+    Ciências: ${ciencias}
+    Sua média final é ${mediaFinal}`)
+
