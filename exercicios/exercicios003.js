4fd10b2e59411556a9e72e28f54149cae8e97a0f
Ana Clara Silva Morais <aninhaatrabalho01@gmail.com>
2026-05-19 23:33:00 -0300

    Exercicios003 finalizado em JS

---
+++ exercicios/exercicios003.js
@@ -0,0 +1,7 @@
+//Crie três variáveis com os dados da sua localização: cidade, estado e pais. Em seguida, mostre no console: "Você está em Cidade - Estado, País.
+
+let cidade = "Valparaiso de Goias"
+let estado = "Goias"
+let pais = "Brasil"
+
+console.log(`Você está em ${cidade} - ${estado}, ${pais}.`)
