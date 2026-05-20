5958de8bb73c774ec0a1336d6dffb9e812cb31e7
Ana Clara Silva Morais <aninhaatrabalho01@gmail.com>
2026-05-20 18:25:00 -0300

    Finalizado exercicio004 em JS

---
+++ exercicios/exercicio004.js
@@ -0,0 +1,4 @@
+//Crie uma variável chamada temCarteira com valor true ou false. Use console.log(typeof temCarteira) para mostrar o tipo dessa variável.
+
+let temCarteira = true;
+console.log(typeof(temCarteira));
