7f4ef9241ba2c7a0bbb1d343ef2a65daab140569
Aninhaxp <aninhaatrabalho01@gmail.com>
2026-06-10 22:45:47 -0300

    Finalizado exercicio 010 em JS

---
+++ exercicios/exercicio010.js
@@ -0,0 +1,8 @@
+//Crie uma variável mensagem contendo uma string e outra numero contendo um número. Junte as duas em uma terceira variável e exiba o resultado e o tipo final no console usando typeof.
+
+let mensagem = "O valor será de R$";
+let valor = 89;
+
+let juncao = mensagem + valor;
+console.log(juncao);
+console.log(typeof juncao);
