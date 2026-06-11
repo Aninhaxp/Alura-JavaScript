520f0f3d4b6a48a142f29bed58b5a4402cb1187d
Aninhaxp <aninhaatrabalho01@gmail.com>
2026-06-10 21:39:42 -0300

    exercicio009 finalizado em JS

---
+++ exercicios/exercicio009.js
@@ -0,0 +1,9 @@
+//Crie uma constante chamada PI com o valor 3.14. Tente alterar seu valor e observe o erro gerado. Depois, explique por que isso acontece.
+
+const numeroPi = 3.14;
+numeroPi = 3.17;
+
+console.log(`${numeroPi}`);
+
+//o erro acontece pq quando se usa uma const declaramos que aquele valor não pode ser alterado.
+
